d72f7ecd9ddeb94e21e9d5da43da889ab684223f
Ivan Quintana <ivanquisco@gmail.com>
2020-08-03 20:17:27 -0500

    initial commit, gitignore

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Ivan Quintana on 03/08/20.
+//  Copyright © 2020 Ivan Quintana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
